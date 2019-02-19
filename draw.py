271be6c64f007657d58b90146efa423d979f82fb
Jaseibert <Jaseibert5@gmail.com>
2019-02-19 10:14:35 -0600

    Second Commit - Created Module Classes, Made Instantiations, and other minor fixes

--- draw.py
+++ draw.py
@@ -3,123 +3,124 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-cos = np.cos
-sin = np.sin
-pi = np.pi
-dot = np.dot
+class Draw(object):
 
-fig = plt.figure(figsize=plt.figaspect(1))
-ax = fig.add_subplot(111, projection='3d', aspect=1)
+    def __init__():
+        return
 
-max_radius = 0
+    fig = plt.figure(figsize=plt.figaspect(1))
+    ax = fig.add_subplot(111, projection='3d', aspect=1)
 
-def plot_earth():
-    "Draw Earth as a globe at the origin"
+    max_radius = 0
 
-    Earth_radius = 6371
-    global max_radius
-    max_radius = max(max_radius, Earth_radius)
+    def plot_earth():
+        "Draw Earth as a globe at the origin"
 
-    # Coefficients in a0/c x**2 + a1/c y**2 + a2/c z**2 = 1
-    coefs = (1, 1, 1)
+        Earth_radius = 6371
+        global max_radius
+        max_radius = max(max_radius, Earth_radius)
 
-    # Radii corresponding to the coefficients:
-    rx, ry, rz = [Earth_radius/np.sqrt(coef) for coef in coefs]
+        # Coefficients in a0/c x**2 + a1/c y**2 + a2/c z**2 = 1
+        coefs = (1, 1, 1)
 
-    # Azimuth Angle in Spherical Coordinates
-    phi = np.linspace(0, 2*np.pi, 100)
+        # Radii corresponding to the coefficients:
+        rx, ry, rz = [Earth_radius/np.sqrt(coef) for coef in coefs]
 
-    # Altitude Angle in Spherical Coordinates
-    theta = np.linspace(0, np.pi, 100)
+        # Azimuth Angle in Spherical Coordinates
+        phi = np.linspace(0, 2*np.pi, 100)
 
-    # Cartesian coordinates that correspond to the spherical angles:
-    #X = r * cos(ϕ)sin(θ)
-    x = rx * np.outer(np.cos(phi), np.sin(theta))
+        # Altitude Angle in Spherical Coordinates
+        theta = np.linspace(0, np.pi, 100)
 
-    #Y = r * cos(ϕ)sin(θ)
-    y = ry * np.outer(np.sin(phi), np.sin(theta))
+        # Cartesian coordinates that correspond to the spherical angles:
+        #X = r * cos(ϕ)sin(θ)
+        x = rx * np.outer(np.cos(phi), np.sin(theta))
 
-    #Z = r * cos(θ)
-    z = rz * np.outer(np.ones_like(phi), np.cos(theta))
-    z = rz *  np.cos(theta)
+        #Y = r * cos(ϕ)sin(θ)
+        y = ry * np.outer(np.sin(phi), np.sin(theta))
 
-    # Plot:
-    ax.plot_surface(x, y, z,  rstride=4, cstride=4, color='b')
+        #Z = r * cos(θ)
+        z = rz * np.outer(np.ones_like(phi), np.cos(theta))
+        #z = rz *  np.cos(theta)
 
-
-def plot_orbit(semi_major_axis, eccentricity=0, inclination=0, right_ascension=0, argument_perigee=0, true_anomaly=0, label=None):
-    "Draws orbit around an earth in units of kilometers."
-
-    # Rotation matrix for inclination
-    inc = inclination * pi / 180
-    R = np.matrix([[1, 0, 0],
-                   [0, cos(inc), -sin(inc)],
-                   [0, sin(inc), cos(inc)]    ])
-
-    # Rotation matrix for argument of perigee + right ascension
-    rot = (right_ascension + argument_perigee) * pi/180
-    R2 = np.matrix([[cos(rot), -sin(rot), 0],
-                    [sin(rot), cos(rot), 0],
-                    [0, 0, 1]    ])
-
-    ### Draw orbit
-    theta = np.linspace(0,2*pi, 360)
-    r = (semi_major_axis * (1-eccentricity**2)) / (1 + eccentricity*cos(theta))
-
-    xr = r*cos(theta)
-    yr = r*sin(theta)
-    zr = 0 * theta
-
-    pts = np.matrix(zip(xr,yr,zr))
-
-    # Rotate by inclination
-    # Rotate by ascension + perigee
-    pts =  (R * R2 * pts.T).T
+        # Plot the Earth:
+        ax.plot_surface(x, y, z,  rstride=4, cstride=4, color='g')
 
 
-    # Turn back into 1d vectors
-    xr,yr,zr = pts[:,0].A.flatten(), pts[:,1].A.flatten(), pts[:,2].A.flatten()
 
-    # Plot the orbit
-    ax.plot(xr, yr, zr, '-')
-    # plt.xlabel('X (km)')
-    # plt.ylabel('Y (km)')
-    # plt.zlabel('Z (km)')
+    def plot_orbit(semi_major_axis, eccentricity=0, inclination=0, right_ascension=0, argument_perigee=0, true_anomaly=0, label=None):
+        "Draws orbit around an earth in units of kilometers."
 
-    # Plot the satellite
-    sat_angle = true_anomaly * pi/180
-    satr = (semi_major_axis * (1-eccentricity**2)) / (1 + eccentricity*cos(sat_angle))
-    satx = satr * cos(sat_angle)
-    saty = satr * sin(sat_angle)
-    satz = 0
+        # Rotation matrix for inclination
+        inc = inclination * np.pi / 180
+        R = np.matrix([[1, 0, 0],
+                       [0, np.cos(inc), -np.sin(inc)],
+                       [0, np.sin(inc), np.cos(inc)]])
 
-    sat = (R * R2 * np.matrix([satx, saty, satz]).T ).flatten()
-    satx = sat[0,0]
-    saty = sat[0,1]
-    satz = sat[0,2]
+        # Rotation matrix for argument of perigee + right ascension
+        rot = (right_ascension + argument_perigee) * np.pi/180
+        R2 = np.matrix([[np.cos(rot), -np.sin(rot), 0],
+                        [np.sin(rot), np.cos(rot), 0],
+                        [0, 0, 1]])
 
-    c = np.sqrt(satx*satx + saty*saty)
-    lat = np.arctan2(satz, c) * 180/pi
-    lon = np.arctan2(saty, satx) * 180/pi
-    print "%s : Lat: %g° Long: %g" % (label, lat, lon)
+        ### Draw orbit
+        theta = np.linspace(0,2*pi, 360)
+        r = (semi_major_axis * (1-eccentricity**2)) / (1 + eccentricity*np.cos(theta))
 
-    # Draw radius vector from earth
-    # ax.plot([0, satx], [0, saty], [0, satz], 'r-')
-    # Draw red sphere for satellite
-    ax.plot([satx],[saty],[satz], 'ro')
+        xr = r*np.cos(theta)
+        yr = r*np.sin(theta)
+        zr = 0 * theta
 
-    global max_radius
-    max_radius = max(max(r), max_radius)
+        pts = np.matrix(zip(xr,yr,zr))
 
-    # Write satellite name next to it
-    if label:
-        ax.text(satx, saty, satz, label, fontsize=12)
+        # Rotate by inclination
+        # Rotate by ascension + perigee
+        pts =  (R * R2 * pts.T).T
 
-def doDraw():
-    # Adjustment of the axes, so that they all have the same span:
-    for axis in 'xyz':
-        getattr(ax, 'set_{}lim'.format(axis))
-        ((-max_radius, max_radius))
 
-    # Draw figure
-    plt.show()
+        # Turn back into 1d vectors
+        xr,yr,zr = pts[:,0].A.flatten(), pts[:,1].A.flatten(), pts[:,2].A.flatten()
+
+        # Plot the orbit
+        ax.plot(xr, yr, zr, '-')
+        # plt.xlabel('X (km)')
+        # plt.ylabel('Y (km)')
+        # plt.zlabel('Z (km)')
+
+        # Plot the satellite
+        sat_angle = true_anomaly * np.pi/180
+        satr = (semi_major_axis * (1-eccentricity**2)) / (1 + eccentricity*np.cos(sat_angle))
+        satx = satr * np.cos(sat_angle)
+        saty = satr * np.sin(sat_angle)
+        satz = 0
+
+        sat = (R * R2 * np.matrix([satx, saty, satz]).T ).flatten()
+        satx = sat[0,0]
+        saty = sat[0,1]
+        satz = sat[0,2]
+
+        c = np.sqrt(satx*satx + saty*saty)
+        lat = np.arctan2(satz, c) * 180/np.pi
+        lon = np.arctan2(saty, satx) * 180/np.pi
+        print "%s : Lat: %g° Long: %g" % (label, lat, lon)
+
+        # Draw radius vector from earth
+        # ax.plot([0, satx], [0, saty], [0, satz], 'r-')
+        # Draw red sphere for satellite
+        ax.plot([satx],[saty],[satz], 'ro')
+
+        global max_radius
+        max_radius = max(max(r), max_radius)
+
+        # Write satellite name next to it
+        if label:
+            ax.text(satx, saty, satz, label, fontsize=12)
+
+    def draw():
+        # Adjustment of the axes, so that they all have the same span:
+        for axis in 'xyz':
+            getattr(ax, 'set_{}lim'.format(axis))
+            ((-max_radius, max_radius))
+
+        # Draw figure
+        plt.show()
